65036b7fb25cea048d499ce407de7f82b4ec9824
MarcoVitoC <marcovito.chr@gmail.com>
2023-05-20 22:18:43 +0700

    add login-register page and registration process

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -3,6 +3,6 @@
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <title>@yield('title')</title>
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-   <link rel="stylesheet" href="{{ asset('css/colors.css') }}">
+   <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
    <link rel="icon" type="image/png" href="{{ asset('images/Librariant.png') }}" />
 </head>
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,15 @@
 <?php
 
+use App\Http\Controllers\GuestController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::middleware(['guest'])->group(function() {
+   Route::get('/', [GuestController::class, 'welcome'])->name('welcome');
 
-Route::get('/', function () {
-   return view('welcome');
-});
+   Route::get('/login', [LoginController::class, 'login'])->name('login');
 
-Route::get('/login', function () {
-   return view('auth.login');
+   Route::get('/register', [RegisterController::class, 'register'])->name('register');
+   Route::post('/register', [RegisterController::class, 'create'])->name('register.create');
 });
