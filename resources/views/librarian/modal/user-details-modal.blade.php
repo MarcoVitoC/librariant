139592985a01245579a01fde882741ae65de84ae
MarcoVitoC <marcovito.chr@gmail.com>
2023-06-28 21:31:49 +0700

    refactor: fix label 'for' value in user-details modal

--- resources/views/librarian/modal/user-details-modal.blade.php
+++ resources/views/librarian/modal/user-details-modal.blade.php
@@ -9,46 +9,46 @@
             <div class="modal-body">
                <div class="col-12">
                   <div class="mb-3">
-                     <label for="isbn" class="col-form-label">Full Name:</label>
+                     <label for="full_name" class="col-form-label">Full Name:</label>
                      <input type="text" class="form-control input-field" value="{{ $user->full_name }}" disabled>
                   </div>
                   <div class="d-flex">
                      <div class="pe-3 mb-3 col-6">
-                        <label for="isbn" class="col-form-label">Username:</label>
+                        <label for="username" class="col-form-label">Username:</label>
                         <input type="text" class="form-control input-field" value="{{ $user->username }}" disabled>
                      </div>
                      <div class="ps-3 mb-3 col-6">
-                        <label for="book_title" class="col-form-label">Email:</label>
+                        <label for="email" class="col-form-label">Email:</label>
                         <input type="text" class="form-control input-field" value="{{ $user->email }}" disabled>
                      </div>
                   </div>
                   <div class="d-flex">
                      <div class="pe-3 mb-3 col-6">
-                        <label for="author" class="col-form-label">Address:</label>
+                        <label for="address" class="col-form-label">Address:</label>
                         <input type="text" class="form-control input-field" value="{{ $user->address }}" disabled>
                      </div>
                      <div class="ps-3 mb-3 col-6">
-                        <label for="pages" class="col-form-label">Phone Number:</label>
+                        <label for="phone_number" class="col-form-label">Phone Number:</label>
                         <input type="text" class="form-control input-field"value="{{ $user->phone_number }}" disabled>
                      </div>
                   </div>
                   <div class="d-flex">
                      <div class="pe-3 mb-3 col-6">
-                        <label for="publisher" class="col-form-label">Date of Birth:</label>
+                        <label for="date_of_birth" class="col-form-label">Date of Birth:</label>
                         <input type="text" class="form-control input-field" value="{{ $user->date_of_birth }}" disabled>
                      </div>
                      <div class="ps-3 mb-3 col-6">
-                        <label for="publish_date" class="col-form-label">Gender:</label>
+                        <label for="gender" class="col-form-label">Gender:</label>
                         <input type="text" class="form-control input-field" value="{{ $user->gender }}" disabled>
                      </div>
                   </div>
                   <div class="d-flex">
                      <div class="pe-3 mb-3 col-6">
-                        <label for="genre" class="col-form-label">Books Borrowed:</label>
+                        <label for="books_borrowed" class="col-form-label">Books Borrowed:</label>
                         <input type="text" class="form-control input-field" value="{{ $user->books_borrowed }}" disabled>
                      </div>
                      <div class="ps-3 mb-3 col-6">
-                        <label for="quantity" class="col-form-label">Penalty:</label>
+                        <label for="penalty" class="col-form-label">Penalty:</label>
                         <input type="text" class="form-control input-field" value="{{ $user->penalty }}" disabled>
                      </div>
                   </div>
