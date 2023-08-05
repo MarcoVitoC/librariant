8cb216c20e08c631be911a29eb51b3bc5f3caa02
MarcoVitoC <marcovito.chr@gmail.com>
2023-08-05 18:03:57 +0700

    style: change admin notification button to dropdown

--- resources/views/layouts/librarian/topbar.blade.php
+++ resources/views/layouts/librarian/topbar.blade.php
@@ -4,8 +4,15 @@
          <span class="navbar-toggler-icon"></span>
       </button>
       <div class="d-flex align-items-center">
-         <div class="border-end pe-3">
-            <a href="" class="text-secondary fs-5"><i class="bi bi-bell-fill"></i></a>
+         <div class="dropdown border-end pe-3">
+            <a href="" class="nav-link text-secondary fs-5" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+               <i class="bi bi-bell-fill"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-end">
+               <a class="dropdown-item py-2" href="">
+                  <i class="bi bi-bell-slash me-2"></i>You have no notifications at the moment.
+               </a>
+            </div>
          </div>
          <div class="dropdown d-flex align-items-center ms-3">
             <a class="nav-link fs-4 text-secondary" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
