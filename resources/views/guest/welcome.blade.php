cc3c4b683669feef48fb83181202ee966ac0ab53
MarcoVitoC <marcovito.chr@gmail.com>
2023-08-10 22:01:43 +0700

    style: fix responsive toggle and revamp the welcome page second section

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -24,7 +24,7 @@
          <span class="fs-3">📗</span>
       </div>
       <div class="row d-flex align-items-center justify-content-center flex-md-row">
-         <div class="text-center col-md-9" style="margin: 9rem 0;">
+         <div class="text-center col-md-9" style="margin: 9rem 0 7rem 0;">
             <h1 class="title-lg fw-bold"><span class="text-underline-wavy">Unleash</span> Your Curiosity: Welcome to <span class="text-highlight">Librariant</span>! 📚</h1>
             <h5 class="fw-normal fs-header text-secondary lh-sm my-4" style="padding: 0px 100px;">Streamline library operations with our user-friendly platform for cataloging, lending, and returning books. Find books easily, place holds, and receive notifications. Explore our platform for an organized and accessible library environment!</h5>
             <a class="btn btn-dark btn-lg exploreBtn" href="{{ route('guest.books') }}" role="button">Explore now</a>
@@ -32,28 +32,35 @@
       </div>
    </div>
    <div class="m-6">
-      <div class="d-flex align-items-baseline justify-content-between px-4">
-         <div class="w-30 text-center border border-3 border-pale rounded p-3" style="height: 210px;">
-            <i class="bi bi-bookshelf fs-1"></i>
-            <h4>Extensive Collection</h4>
-            <p>Our library boasts a vast and diverse collection of books, articles, and resources, catering to a wide range of interests and disciplines.</p>
-         </div>
-         <div class="w-30 text-center border border-3 border-pale rounded p-3" style="height: 210px;">
-            <i class="bi bi-globe fs-1"></i>
-            <h4>Easy Accessbility</h4>
-            <p>Our website provides convenient access to our library's resources from anywhere, at any time, with just a few clicks.</p>
-         </div>
-         <div class="w-30 text-center border border-3 border-pale rounded p-3" style="height: 210px;">
-            <i class="bi bi-columns-gap fs-1"></i>
-            <h4>User-Friendly Interface</h4>
-            <p>Our intuitive interface and search functionality make it easy for users to navigate and find the materials they need quickly and efficiently.</p>
-         </div>
+      <h2 class="text-center pt-5 pb-4">Our Features & Services</h2>
+      <div class="row px-4">
+         <div class="col-md-4 text-center">
+            <div class="d-flex flex-column align-items-center h-100 bg-body-tertiary rounded p-3">
+               <img src="{{ asset('images/Extensive Collection.png') }}" alt="Extensive Collection" class="mw-100 h-auto" width="250px" height="250px">
+               <h4>Extensive Collection</h4>
+               <p>Our library boasts a vast and diverse collection of books, articles, and resources, catering to a wide range of interests and disciplines.</p>
+            </div>
+        </div>
+        <div class="col-md-4 text-center">
+            <div class="d-flex flex-column align-items-center h-100 bg-body-tertiary rounded p-3">
+               <img src="{{ asset('images/Easy Accessibility.png') }}" alt="Easy Accessibility" class="mw-100 h-auto" width="250px" height="250px">
+               <h4>Easy Accessibility</h4>
+               <p>Our website provides convenient access to our library's resources from anywhere, at any time, with just a few clicks.</p>
+            </div>
+        </div>
+        <div class="col-md-4 text-center">
+            <div class="d-flex flex-column align-items-center h-100 bg-body-tertiary rounded p-3">
+               <img src="{{ asset('images/User Friendly Interface.png') }}" alt="User Friendly Interface" class="mw-100 h-auto" width="250px" height="250px">
+               <h4>User-Friendly Interface</h4>
+               <p>Our intuitive interface and search functionality make it easy for users to navigate and find the materials they need quickly and efficiently.</p>
+            </div>
+        </div>
       </div>
    </div>
    <div class="m-6">
       <div class="d-flex align-items-baseline justify-content-between mb-3 px-4">
          <h2 class="fw-medium">Popular Books</h2>
-         <a href="{{ route('guest.books') }}" class="fw-medium cursor-pointer link-dark link-offset-1 link-underline-opacity-0 link-underline-opacity-100-hover">See all</a>
+         <a href="{{ route('guest.books') }}" class="fw-medium cursor-pointer link-dark link-offset-1 link-underline-opacity-0 link-underline-opacity-100-hover">View all</a>
       </div>
       <div class="d-flex justify-content-between px-4">
          @foreach ($books as $book)
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -5,66 +5,69 @@
          <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse mx-2" id="navbarNavAltMarkup">
-         <div class="navbar-nav mx-2">
-         @if (!Auth::check())
-            <a class="nav-link {{ Route::currentRouteNamed('guest.welcome') ? 'active' : '' }} mx-3 fw-semibold" href="{{ route('guest.welcome') }}">Home</a>
-            <a class="nav-link {{ Route::currentRouteNamed('guest.books') ? 'active' : '' }} mx-3 fw-semibold" href="{{ route('guest.books') }}">Books</a>
-         @else
-            <a class="nav-link {{ Route::currentRouteNamed('user.home') ? 'active' : '' }} mx-3 fw-semibold" href="{{ route('user.home') }}">Home</a>
-         @endif
-            <a class="nav-link {{ Route::currentRouteNamed('visitor.about_us') ? 'active' : '' }} mx-3 fw-semibold" href="{{ route('visitor.about_us') }}">About</a>
-            <a class="nav-link {{ Route::currentRouteNamed('visitor.faq') ? 'active' : '' }} mx-3 fw-semibold" href="{{ route('visitor.faq') }}">FAQ</a>
-         </div>
-      </div>
-      @if (!Auth::check())
-         <a class="btn btn-dark" href="/login" role="button">Log in</a>
-      @else
-      <div class="d-flex align-items-center">
-         <div class="dropdown">
-            @if ($notifications->isEmpty())
-               <button class="nav-link text-secondary fs-4 position-relative" data-bs-toggle="dropdown">
-                  <i class="bi bi-bell-fill"></i>
-               </button>
-               <div class="dropdown-menu dropdown-menu-end">
-                  <a class="dropdown-item py-2" href="">
-                     <i class="bi bi-bell-slash me-2"></i>You have no notifications at the moment.
-                  </a>
-               </div>
+         <div class="d-flex align-items-center justify-content-between w-100 mx-2">
+            <div class="navbar-nav">
+               @if (!Auth::check())
+                  <a class="nav-link {{ Route::currentRouteNamed('guest.welcome') ? 'active' : '' }} mx-3 fw-semibold" href="{{ route('guest.welcome') }}">Home</a>
+                  <a class="nav-link {{ Route::currentRouteNamed('guest.books') ? 'active' : '' }} mx-3 fw-semibold" href="{{ route('guest.books') }}">Books</a>
+               @else
+                  <a class="nav-link {{ Route::currentRouteNamed('user.home') ? 'active' : '' }} mx-3 fw-semibold" href="{{ route('user.home') }}">Home</a>
+               @endif
+               <a class="nav-link {{ Route::currentRouteNamed('visitor.about_us') ? 'active' : '' }} mx-3 fw-semibold" href="{{ route('visitor.about_us') }}">About</a>
+               <a class="nav-link {{ Route::currentRouteNamed('visitor.faq') ? 'active' : '' }} mx-3 fw-semibold" href="{{ route('visitor.faq') }}">FAQ</a>
+            </div>
+
+            @if (!Auth::check())
+               <a class="btn btn-dark" href="{{ route('login') }}" role="button">Log in</a>
             @else
-               <button class="nav-link text-secondary fs-4 position-relative" data-bs-toggle="dropdown">
-                  <i class="bi bi-bell-fill"></i>
-                  <span class="badge rounded-pill bg-danger badge-size">
-                     {{ $notifications->count() }}
-                  </span>
-               </button>
-               <div class="dropdown-menu dropdown-menu-end">
-                  @foreach ($notifications as $notification)
-                     <div class="dropdown-item py-2">{{ $notification->content }}</div>
-                  @endforeach
+               <div class="d-flex align-items-center">
+                  <div class="dropdown">
+                     @if ($notifications->isEmpty())
+                        <button class="nav-link text-secondary fs-4 position-relative" data-bs-toggle="dropdown">
+                           <i class="bi bi-bell-fill"></i>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-end">
+                           <a class="dropdown-item py-2" href="">
+                              <i class="bi bi-bell-slash me-2"></i>You have no notifications at the moment.
+                           </a>
+                        </div>
+                     @else
+                        <button class="nav-link text-secondary fs-4 position-relative" data-bs-toggle="dropdown">
+                           <i class="bi bi-bell-fill"></i>
+                           <span class="badge rounded-pill bg-danger badge-size">
+                              {{ $notifications->count() }}
+                           </span>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-end">
+                           @foreach ($notifications as $notification)
+                              <div class="dropdown-item py-2">{{ $notification->content }}</div>
+                           @endforeach
+                        </div>
+                     @endif
+                  </div>
+                  <div class="vr mx-3 text-secondary"></div>
+                  <div class="dropdown">
+                     <a class="nav-link fs-4 text-secondary" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="bi bi-person-circle"></i>
+                     </a>
+                     <div class="dropdown-menu dropdown-menu-end">
+                        <a class="dropdown-item py-2" href="{{ route('user.profile') }}">
+                           <i class="bi bi-person me-2"></i>My Profile
+                        </a>
+                        <a class="dropdown-item py-2" href="{{ route('user.bookmarks') }}">
+                           <i class="bi bi-bookmarks me-2"></i>Bookmarks
+                        </a>
+                        <a class="dropdown-item py-2" href="{{ route('user.loans') }}">
+                           <i class="bi bi-book me-2"></i>Loans
+                        </a>
+                        <a class="dropdown-item py-2 text-danger" href="{{ route('logout') }}">
+                           <i class="bi bi-box-arrow-right me-2 text-danger"></i>Log out
+                        </a>
+                     </div>
+                  </div>
                </div>
             @endif
          </div>
-         <div class="vr mx-3 text-secondary"></div>
-         <div class="dropdown">
-            <a class="nav-link fs-4 text-secondary" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-               <i class="bi bi-person-circle"></i>
-            </a>
-            <div class="dropdown-menu dropdown-menu-end">
-               <a class="dropdown-item py-2" href="{{ route('user.profile') }}">
-                  <i class="bi bi-person me-2"></i>My Profile
-               </a>
-               <a class="dropdown-item py-2" href="{{ route('user.bookmarks') }}">
-                  <i class="bi bi-bookmarks me-2"></i>Bookmarks
-               </a>
-               <a class="dropdown-item py-2" href="{{ route('user.loans') }}">
-                  <i class="bi bi-book me-2"></i>Loans
-               </a>
-               <a class="dropdown-item py-2 text-danger" href="{{ route('logout') }}">
-                  <i class="bi bi-box-arrow-right me-2 text-danger"></i>Log out
-               </a>
-            </div>
-         </div>
       </div>
-      @endif
    </div>
 </nav>
