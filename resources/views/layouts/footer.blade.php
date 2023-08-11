acbe56ef7079532f4c782cda8783267960913091
MarcoVitoC <marcovito.chr@gmail.com>
2023-08-11 22:10:24 +0700

    style: revamp footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,32 +1,13 @@
-<footer class="bg-cornsilk mt-5 px-4">
-   <div class="d-flex align-items-center justify-content-between mx-6 pt-3 pb-4">
-      <div>
+<footer class="mt-5">
+   <hr class="text-secondary">
+   <div class="mx-6 px-4">
+      <div class="d-flex align-items-center justify-content-between pt-3 pb-4">
+         <h6 class="text-center fw-medium text-secondary">&copy; 2023 Librariant. All Rights Reserved.</h6>
          <div class="d-flex">
-            <i class="bi bi-clock-fill me-2"></i><p>Monday - Saturday, from 9:00 a.m. to 6:00 p.m.</p>
+            <a href="https://www.facebook.com/" target="_blank" class="facebook bg-body-secondary rounded-circle mx-2 px-2 py-1"><i class="bi bi-facebook text-secondary fs-5"></i></a>
+            <a href="https://twitter.com/" target="_blank" class="twitter bg-body-secondary rounded-circle mx-2 px-2 py-1"><i class="bi bi-twitter text-secondary fs-5"></i></a>
+            <a href="https://www.instagram.com/" target="_blank" class="instagram bg-body-secondary rounded-circle mx-2 px-2 py-1"><i class="bi bi-instagram text-secondary fs-5"></i></a>
          </div>
-         <div class="d-flex">
-            <i class="bi bi-geo-alt-fill me-2"></i></i><p>Education Street No.10B</p>
-         </div>
-         <div class="d-flex">
-            <i class="bi bi-telephone-fill me-2"></i></i><p>+6281-123-456-789</p>
-         </div>
-         <div class="d-flex">
-            <i class="bi bi-envelope-fill me-2"></i></i><p>librariant.edu@gmail.com</p>
-         </div>
-      </div>
-      <div>
-         <div class="d-flex justify-content-center pb-3">
-            <a href="https://www.facebook.com/" target="_blank"><i class="bi bi-facebook text-dark fs-4 me-2"></i></a>
-            <a href="https://twitter.com/" target="_blank"><i class="bi bi-twitter text-dark fs-4 mx-2"></i></a>
-            <a href="https://www.instagram.com/" target="_blank"><i class="bi bi-instagram text-dark fs-4 mx-2"></i></a>
-            <a href="https://www.linkedin.com/" target="_blank"><i class="bi bi-linkedin text-dark fs-4 mx-2"></i></a>
-         </div>
-         <h6 class="text-center fw-medium mb-1">Privacy Policy | Terms of Use</h6>
-         <h6 class="text-center fw-medium">&copy; 2023 Librariant. All Rights Reserved.</h6>
-      </div>
-      <div class="w-25">
-         <img src="{{ asset('images/Librariant Logo (Light).png') }}" alt="Librariant" width="160px" height="50px">
-         <p class="ps-3">Discover a world of knowledge at our library. We offer a vast collection of books, magazines, and e-books to fuel your curiosity.</p>
       </div>
    </div>
 </footer>
