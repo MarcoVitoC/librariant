fea4a1526b9774714e811f5f98f1e5f2cbabf21d
MarcoVitoC <marcovito.chr@gmail.com>
2023-08-24 09:33:46 +0700

    feat: delete all stored files in storage after seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,12 +3,15 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
    public function run()
    {
+      Storage::deleteDirectory('book-photos');
+      
       $this->call([
          UserSeeder::class,
          RoleSeeder::class,
